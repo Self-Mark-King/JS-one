acebd1cd1149890b151f23b3d531b02d35daf041
Ceiphid <Aqkeem@gmail.com>
2019-08-14 20:39:27 -0700

    take home day 2

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let firstName = prompt('wats ur First name?');
+let lastName = prompt('wats ur Last name?');
+console.log(firstName);
+alert(lastName);
+let bDay = prompt('what\'s your birthday?');
+let bol = confirm(`is your birthday?  ${bDay}`);
+console.log(typeof(bDay));
+let dateBDay = new date(bDay);
