9696e693916253a7334fffc9c7ace55a60a87a90
Ceiphid <Aqkeem@gmail.com>
2019-08-16 16:04:16 -0700

    work from class three

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let anyString = "what String";
+new String("whatString");
+if (anyString == "whatString"){
+    console.log('strings are equal');
+}   else{
+    console.log('the strings are NOT equal')
+}
+
+let txt = "some text";
+let len = txt.length;
+console.log(`the length is:  ${len}`);
+numberUno = prompt('please enter a number', 10);
+numberDos = prompt('please eneter a second number', 10);
+numberUno = Number(numberUno);
+numberDos = Number(numberDos);
+alert(numberUno + numberDos);
