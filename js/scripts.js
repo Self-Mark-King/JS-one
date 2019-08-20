8ed46284d4b9c3058ed3156b74b955ac65a665e3
Ceiphid <Aqkeem@gmail.com>
2019-08-19 18:04:41 -0700

    homework

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,105 @@
-let anyString = "what String";
-new String("whatString");
-if (anyString == "whatString"){
-    console.log('strings are equal');
-}   else{
-    console.log('the strings are NOT equal')
+// .........................................
+// IN class exercise 1
+// .............................................
+
+
+// let anyString = "what String";
+// new String("whatString");
+// if (anyString == "whatString"){
+//     console.log('strings are equal');
+// }   else{
+//     console.log('the strings are NOT equal')
+// }
+
+// let txt = "some text";
+// let len = txt.length;
+// console.log(`the length is:  ${len}`);
+
+
+// numberUno = prompt('please enter a number', 10);
+// numberDos = prompt('please eneter a second number', 10);
+// numberUno = Number(numberUno);
+// numberDos = Number(numberDos);
+// alert(numberUno + numberDos);
+
+// ..............................
+
+// ..............................
+// in class exercise 2
+// .................................
+
+// let oneEight = confirm('Are you 18 or over?');  
+
+// function mayContinue(oneEight){
+//     return(oneEight ? eighty() : alert('Your NOT 18!'));
+// }
+// mayContinue(oneEight);
+
+
+// function eighty(eightZero){
+//     eightZero = confirm('Are you bewteen 18 and 80 years old?');
+//     return(eightZero ? confirm('do you like Star Wars?') : confirm('Do you like plums?'));
+// }
+
+// .............
+
+// ...........................................
+// Day Three take home
+// ...........................................
+let name = prompt("Wat's your name");
+let color = prompt("Choose your favourite color: Green, Red, Orange, Yellow, Blue, Pink, Purple, Black, White, Grey");
+
+// if (typeof color != "string") {
+//     alert('Enter a String MORON!');
+// } else {
+//     alert('message');
+// }
+
+if (color = 'Red'||'Green'||'Orange'||'Yellow'||'Blue'||'Pink'||'Purple'||'Black'||'White'||'Grey'){
+    
+} else  {
+    alert('Enter a color!')
 }
 
-let txt = "some text";
-let len = txt.length;
-console.log(`the length is:  ${len}`);
-numberUno = prompt('please enter a number', 10);
-numberDos = prompt('please eneter a second number', 10);
-numberUno = Number(numberUno);
-numberDos = Number(numberDos);
-alert(numberUno + numberDos);
+
+switch(color) {
+    case color = 'Red':
+        alert(`${name}: Your Favorite color is ${color}`);
+        break;
+    case color = 'Green':
+            alert(`${name}: Your Favorite color is ${color}`);
+            break;    
+    case color = 'Orange':
+            alert(`${name}: Your Favorite color is ${color}`);
+            break;
+    case color = 'Yellow':
+            alert(`${name}: Your Favorite color is ${color}`);
+            break;
+    case color = 'Blue':
+            alert(`${name}: Your Favorite color is ${color}`);
+            break;
+    case color = 'Blue':
+            alert(`${name}: Your Favorite color is ${color}`);
+            break;
+    case color = 'Pink':
+            alert(`${name}: Your Favorite color is ${color}`);
+            break;
+    case color = 'Purple':
+            alert(`${name}: Your Favorite color is ${color}`);
+            break;
+    case color = 'Black':
+        alert(`${name}: Your Favorite color is ${color}`);
+        break;
+    case color = 'White':
+        alert(`${name}: Your Favorite color is ${color}`);
+        break;
+    case color = 'Grey':
+        alert(`${name}: your Favorite color is ${color}`);
+        break;
+
+}
+
+// ${name}
+
+// ............................................
+
