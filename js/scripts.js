636384a743d4543be92bbbd6cad329a4633e7f60
Ceiphid <Aqkeem@gmail.com>
2019-09-09 13:56:09 -0700

    take home

--- js/scripts.js
+++ js/scripts.js
@@ -129,76 +129,95 @@
 // form.setAttribute('action', 'form-submission.js');
 // form.setAttribute('name', 'myForm');
 
-// ------------------ jacques -------------------------
-function globalFormElements(element, elementType, elementName, elementClass, elementPlaceholder, elementId) {
-    //element : create the type of element you seek
-    const newElement = document.createElement(element);
-    newElement.setAttribute('type', elementType);
-    newElement.className = elementClass;
-    newElement.setAttribute('name', elementName);
-    newElement.setAttribute('placeholder', elementPlaceholder);
-    newElement.id = elementId;
-    return newElement;
-}
-const textAreaElement = (name, placeholder, elementClass, elementId, cols = 4, rows = 2) => {
-    const element = document.createElement('textarea');
-    element.setAttribute('type', 'text');
-    element.className = elementClass;
-    element.setAttribute('name', name);
-    element.setAttribute('placeholder', placeholder);
-    element.setAttribute('cols', cols);
-    element.setAttribute('rows', rows);
-    element.id = elementId;
-    return element;
-}
+// // ------------------ jacques -------------------------
+// function globalFormElements(element, elementType, elementName, elementClass, elementPlaceholder, elementId) {
+//     //element : create the type of element you seek
+//     const newElement = document.createElement(element);
+//     newElement.setAttribute('type', elementType);
+//     newElement.className = elementClass;
+//     newElement.setAttribute('name', elementName);
+//     newElement.setAttribute('placeholder', elementPlaceholder);
+//     newElement.id = elementId;
+//     return newElement;
+// }
+// const textAreaElement = (name, placeholder, elementClass, elementId, cols = 4, rows = 2) => {
+//     const element = document.createElement('textarea');
+//     element.setAttribute('type', 'text');
+//     element.className = elementClass;
+//     element.setAttribute('name', name);
+//     element.setAttribute('placeholder', placeholder);
+//     element.setAttribute('cols', cols);
+//     element.setAttribute('rows', rows);
+//     element.id = elementId;
+//     return element;
+// }
 
-const labelGenerator = (forElement, text) => {
-    const label = document.createElement('label');
-    label.setAttribute('for', forElement);
-    label.innerText = text;
-    return label; 
-}
-console.log('this is an element: ',globalFormElements('input', 'text', 'example', 'form-control bg-primary', 'Type your example here'));
+// const labelGenerator = (forElement, text) => {
+//     const label = document.createElement('label');
+//     label.setAttribute('for', forElement);
+//     label.innerText = text;
+//     return label; 
+// }
+// console.log('this is an element: ',globalFormElements('input', 'text', 'example', 'form-control bg-primary', 'Type your example here'));
 
-console.log('this is a text-area element: ',globalFormElements('textarea', 'text', 'message', 'bg-danger', 'Please, let us know your concerns here.' ));
+// console.log('this is a text-area element: ',globalFormElements('textarea', 'text', 'message', 'bg-danger', 'Please, let us know your concerns here.' ));
 
-const form = document.createElement('form');
-const inputEmail = globalFormElements('input', 'email', 'email', 'form-control', 'Enter email here');
-const inputSubject = globalFormElements('input', 'text', 'subject', 'form-control', 'Enter Subject here');
-const textArea = textAreaElement('message', 'Your message here...', 'form-control', 'message', 5, '');
-const button = document.createElement('button');
-button.className = 'btn-primary btn';
-button.setAttribute('type', 'submit');
-button.innerText = 'Submit Now';
 
-const labelForEmail = labelGenerator('email', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, dolor?');
+// // 
+// const form = document.createElement('form');
+// const inputEmail = globalFormElements('input', 'email', 'email', 'form-control', 'Enter email here');
+// const inputSubject = globalFormElements('input', 'text', 'subject', 'form-control', 'Enter Subject here');
+// const textArea = textAreaElement('message', 'Your message here...', 'form-control', 'message', 5, '');
+// const button = document.createElement('button');
+// button.className = 'btn-primary btn';
+// button.setAttribute('type', 'submit');
+// button.innerText = 'Submit Now';
 
-const labelForSubject = labelGenerator('subject', 'Subject');
-const labelForTextArea = labelGenerator('message', 'Message ');
+// const labelForEmail = labelGenerator('email', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, dolor?');
 
-const formGroup = document.createElement('div');
-formGroup.className = 'form-group';
+// const labelForSubject = labelGenerator('subject', 'Subject');
+// const labelForTextArea = labelGenerator('message', 'Message ');
 
-//append labels to inputs
+// const formGroup = document.createElement('div');
+// formGroup.className = 'form-group';
 
-formGroup.appendChild(labelForEmail);
-formGroup.appendChild(inputEmail);
-formGroup.appendChild(labelForSubject);
-formGroup.appendChild(inputSubject);
-formGroup.appendChild(labelForTextArea);
-formGroup.appendChild(textArea);
-formGroup.appendChild(button);
+// //append labels to inputs
 
-form.appendChild(formGroup);
+// formGroup.appendChild(labelForEmail);
+// formGroup.appendChild(inputEmail);
+// formGroup.appendChild(labelForSubject);
+// formGroup.appendChild(inputSubject);
+// formGroup.appendChild(labelForTextArea);
+// formGroup.appendChild(textArea);
+// formGroup.appendChild(button);
 
-form.setAttribute('method', 'GET');
-form.setAttribute('action', 'form-submission.js');
-form.setAttribute('name', 'myForm');
+// form.appendChild(formGroup);
 
-let myelememt = document.getElementById('id1');
-myelememt.appendChild(form);
+// form.setAttribute('method', 'GET');
+// form.setAttribute('action', 'form-submission.js');
+// form.setAttribute('name', 'myForm');
+
+// let myelememt = document.getElementById('id1');
+// myelememt.appendChild(form);
 
 // --------------------- end Jacques --------------------
+
+
+//-----------begin mark ---------------------
+// 
+// original below ---- arrow functions rewritten below
+
+// function globalFormElements(element, elementType, elementName, elementClass, elementPlaceholder, elementId) {
+//     //element : create the type of element you seek
+//     const newElement = document.createElement(element);
+//     newElement.setAttribute('type', elementType);
+//     newElement.className = elementClass;
+//     newElement.setAttribute('name', elementName);
+//     newElement.setAttribute('placeholder', elementPlaceholder);
+//     newElement.id = elementId;
+//     return newElement;
+// }
+
 
 // const textAreaElement = (name, placeholder, elementClass, elementId, cols = 4, rows = 2) => {
 //     const element = document.createElement('textarea');
@@ -218,3 +237,97 @@ myelememt.appendChild(form);
 //     label.innerText = text;
 //     return label; 
 // }
+
+
+//function to create elements
+function globalFormElements(element, elementType, elementName, elementClass, elementPlaceholder, elementId) {
+    //element : create the type of element you seek
+    const newElement = document.createElement(element);
+    newElement.setAttribute('type', elementType);
+    newElement.className = elementClass;
+    newElement.setAttribute('name', elementName);
+    newElement.setAttribute('placeholder', elementPlaceholder);
+    newElement.id = elementId;
+    return newElement;
+}
+
+//function to create labels
+function labelGenerator(forElement, text) {
+    const label = document.createElement('label');
+    label.setAttribute('for', forElement);
+    label.innerText = text;
+    return label; 
+}
+
+// function to create text area element
+function textAreaElement(name, placeholder, elementClass, elementId, cols = 4, rows = 2){
+    const element = document.createElement('textarea');
+    element.setAttribute('type', 'text');
+    element.className = elementClass;
+    element.setAttribute('name', name);
+    element.setAttribute('placeholder', placeholder);
+    element.setAttribute('cols', cols);
+    element.setAttribute('rows', rows);
+    element.id = elementId;
+    return element;
+}
+
+//create form function
+function createForm(){
+    const element = document.createElement('form');
+    element.setAttribute('method', 'GET');
+    element.setAttribute('action', 'form-submission.js');
+    element.setAttribute('name', 'myForm');
+    return element;
+}
+
+//append stuff function
+function appendStuff(){
+    thisForm.appendChild(lEmail);
+    thisForm.appendChild(iEmail);
+    thisForm.appendChild(lSubject);
+    thisForm.appendChild(iSubject);
+    thisForm.appendChild(lTextArea);
+    thisForm.appendChild(iBody);
+    thisForm.appendChild(button);
+    }
+
+
+// create form
+const thisForm = createForm();
+
+//create inputs
+const iEmail = globalFormElements('input','input','email','form-control','enter email address here');
+const iSubject = globalFormElements('input', 'text', 'subject', 'form-control', 'Enter subject here');
+const iBody = textAreaElement('message', 'Your message here', 'form-control','message',5,'');
+
+//create labels
+const lEmail = labelGenerator('email','Lorem ipsum dolor sit  adipisicing elit. Debitis, dolor?');
+const lSubject = labelGenerator('subject', 'Subject');
+const lTextArea = labelGenerator('message', 'Message ');
+
+//create button
+const button = document.createElement('button');
+button.className = 'btn-primary btn';
+button.setAttribute('type', 'submit');
+button.innerText = 'Submit Now';
+
+
+
+
+//put stuff together
+appendStuff();
+
+//wrap up and put elements into index
+let myelememt = document.getElementById('id1');
+myelememt.appendChild(thisForm);
+
+
+
+
+
+
+
+
+
+
