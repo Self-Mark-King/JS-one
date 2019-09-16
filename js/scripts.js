af7c58d9fae7ee375eb25c36521c690eaa25fac6
Ceiphid <Aqkeem@gmail.com>
2019-09-16 15:24:22 -0700

    more object practice

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,19 @@ console.log(myName.lastName);
 const pElement = document.getElementById('demo').innerHTML = "this is the element you've been looking for and my first name is: " + myName.firstName;
 
 // example of not being able to add a property  to an object created with a constructor
-myTest.otherName = "Sam";
-console.log(myName.otherName);
 
-// console.log(myTest);
+// myTest.otherName = "Sam";
+// console.log(myName.otherName);
+
+// adding a property using prototype
+myTest.prototype.otherName = this.otherName;
+console.log(myTest);
+
+let addingAPrototype = new myTest('Cody', 'King', 'CODE');
+console.log(addingAPrototype.otherName);
+addingAPrototype.otherName = "codE";
+console.log(addingAPrototype.otherName);
+
+
+const lastElement = document.getElementById('demo');
+lastElement.appendChild('p').innerHTML = addingAPrototype.otherName;
